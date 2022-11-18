7a63078ef99337b26e3ad8f95235fd3538ab82af
EarlofSmith <r1smith3333@gmail.com>
2022-11-17 18:17:53 -0700

    working on css weather fech

--- assets/style.js
+++ assets/style.js
@@ -9,16 +9,29 @@ class weather{
     const myKey = "e229d0779c3492fe78bb28175e18dbe9";
     // converts city choice into latitude and longitude coordinates
     var location = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${myKey}`);
+    console.log(location)
     // stores input as a json
-    var coordinates = await location.JSON();
-
+    var coordinates = await location.json();
+console.log(coordinates)
     const forecast = await fetch(
         `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0].lat}&lon=${coordinates[0].lon}&appid=${myKey}&units=imperial`
       );
-
+        console.log(forecast)
     var storedForecast = await forecast.JSON();
     console.log(storedForecast);
     return storedForecast;
     }
 }
-weather();
+
+var W =new weather();
+var search = document.getElementById("Search-bar");
+var button = document.getElementById("search-btn");
+button.addEventListener("click", () => {
+    const current = search.value;
+    search.value = '';
+    W.fetchWeather(current).then((data) => {
+        console.log(data)
+    //   display.displayData(data);
+    //   display.localStorageSet(data);
+    });
+  });
